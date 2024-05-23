89959a65573251209f7ac2643d9fcdddcb94e2c3
Githi54 <chinter.games1@gmail.com>
2024-05-23 22:34:35 +0300

    migrations

--- taxi/models.py
+++ taxi/models.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Manufacture(models.Model):
+class Manufacturer(models.Model):
     name = models.CharField(max_length=80, unique=True)
-    country = models.CharField()
+    country = models.CharField(max_length=80)
     
     def __str__(self) -> str:
         return f"{self.name}: {self.country}"
@@ -18,9 +18,9 @@ class Driver(AbstractUser):
     
 class Car(models.Model):
     model = models.CharField(max_length=95, unique=True)
-    manufacturer = models.ForeignKey(Manufacture, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
     
     class Meta:
-        ordering = ("models",)
+        ordering = ("model",)
     
