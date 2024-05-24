b8592df4864c7577ac5c74321546a5748521e7a7
Githi54 <chinter.games1@gmail.com>
2024-05-24 17:45:46 +0300

    � improve code style

--- taxi/models.py
+++ taxi/models.py
@@ -2,25 +2,26 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class Manufacturer(models.Model):
     name = models.CharField(max_length=80, unique=True)
     country = models.CharField(max_length=80)
-    
+
     def __str__(self) -> str:
         return f"{self.name}: {self.country}"
-    
+
+
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=80, unique=True)
-    
+
     class Meta:
         ordering = ("username",)
-    
-    
+
+
 class Car(models.Model):
     model = models.CharField(max_length=95, unique=True)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
-    
+
     class Meta:
         ordering = ("model",)
-    
